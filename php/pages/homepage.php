f6526069bc1d3197cffc99cbfa5b7a690accf742
Olpouin <olpouin@live.fr>
2019-03-23 01:02:06 +0100

    📄 Homepage

--- php/pages/homepage.php
+++ php/pages/homepage.php
@@ -1,5 +1,6 @@
 <?php
 $infoContent['g_title'] = "Accueil";
+$content['hp']['search'] = $content['hp']['rand'] = $content['hp']['langForm'] = "";
 //Main text
 $homepageReq = $db->prepare("SELECT * FROM {$config['database']['table']} WHERE type = '[SERVERDATA]' AND name = 'homepage'");
 $homepageReq->execute();
@@ -7,28 +8,26 @@ $homepage = $homepageReq->fetch();
 $homepage = $format($homepage['text'], false);
 //Search
 if (isset($_GET['search'])) {
-$content['hp']['search'] = "<div class='center'>";
+	$content['hp']['search'] = "<div class='center'>";
 	$searchDB = $db->prepare("SELECT * FROM {$config['database']['table']} WHERE (name REGEXP ? OR groupe REGEXP ?) AND hidden = 0");
 	$searchDB->execute(array($_GET['search'],$_GET['search']));
 	while ($listing = $searchDB->fetch()) {
 		$content['hp']['search'] .= $previewBox($listing);
 	}
-$content['hp']['search'] .= "</div>";
-} else $content['hp']['search'] = "";
+	$content['hp']['search'] .= "</div>";
+}
 //4 random cards
 $totalDBCounter = $db->query("select count(*) from {$config['database']['table']} where hidden = '0'")->fetchColumn();
-$content['hp']['rand'] = "<div class='center'><h2>".str_replace('[TOTALPAGES]', $totalDBCounter, $lang['homepage-top_message'])."</h2>";
+$content['hp']['randTitle'] = str_replace('[TOTALPAGES]', $totalDBCounter, $lang['homepage-top_message']);
 $boxList = $db->prepare("SELECT * FROM {$config['database']['table']} WHERE hidden = 0 ORDER BY rand() LIMIT 4");
 $boxList->execute();
 while ($listing = $boxList->fetch()) {
 	$content['hp']['rand'] .= $previewBox($listing);
 }
-$content['hp']['rand'] .= "</div>";
 
-$langForm = "";
 foreach ($config['lang'] as $key => $value) {
 	$langSelected = ($key == $config['general']['default_language']) ? "selected='selected'" : "";
-	$langForm .= "<option {$langSelected} value='{$key}'>{$value}</option>";
+	$content['hp']['langForm'] .= "<option {$langSelected} value='{$key}'>{$value}</option>";
 }
 
 //HTML
@@ -40,7 +39,10 @@ $content['card'] = <<<HOMEPAGE
 	<input class="cardSearch-button" type="submit" value="{$lang['homepage-search_button']}">
 </form>
 {$content['hp']['search']}
-{$content['hp']['rand']}
+<div class='center'>
+	<h2>{$content['hp']['randTitle']}</h2>
+	{$content['hp']['rand']}
+</div>
 <div class="center">
 	<h2 id="pref">{$lang['homepage-prefs-title']}</h2>
 </div>
@@ -48,8 +50,7 @@ $content['card'] = <<<HOMEPAGE
 	<input class="checkbox" id="nightmode" type="checkbox" value="on">
 	<label for="nightmode" class="toggle">{$lang['homepage-prefs-nightmode']}</label><br><br>
 	<select id="pref-chooseLang">
-		<option value="en_US">English, US</option>
-		<option value="fr_FR">Français, France</option>
+		{$content['hp']['langForm']}
 	</select>
 	<label for="pref-chooseLang">{$lang['language']}</label><br><br>
 	<input class="checkbox" id="prefeditor" type="checkbox" value="on">
