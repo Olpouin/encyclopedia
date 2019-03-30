794b8f2e47c7a52f526082b9791b2e734556358a
Olpouin <olpouin@live.fr>
2019-03-30 17:59:30 +0100

    🔀 Removed stats from admin panel

--- php/pages/homepage-edit.php
+++ php/pages/homepage-edit.php
@@ -17,56 +17,7 @@ if (isset($_POST['pass'])) {
 		foreach ($config['types'] as $key => $value) {
 			$typesForm .= '<option value="'.$key.'">'.ucfirst($value).'</option>';
 		}
-		$cards = $db->prepare("SELECT * FROM {$config['database']['table']}");
-		$cards->execute();
-		$stats['c'] = $stats['t'] = "";
-		$stats['g']['length'] = $stats['g']['img'] = $stats['g']['vid'] = $stats['g']['cards'] = $stats['g']['words'] = 0;
-		while ($card = $cards->fetch()) {
-			$stats['g']['length'] += strlen($card['text']);
-			$stats['g']['words'] += str_word_count($card['text']);
-			preg_match_all('/\!\[.*\]\(.*\)/Um', $card['text'], $imgMatches);
-			$stats['g']['img'] += count($imgMatches[0]);
-			preg_match_all('/\!\(https?\:\/\/www\.youtube\.com\/watch\?v\=(.*)\)/Um', $card['text'], $vidMatches);
-			$stats['g']['vid'] += count($vidMatches[0]);
-			$stats['g']['cards'] += 1;
-			if (isset($stats['g']['t'][$card['type']])) $stats['g']['t'][$card['type']] += strlen($card['text']);
-			else $stats['g']['t'][$card['type']] = strlen($card['text']);
-		}
-		$stats['g']['length-moy'] = round($stats['g']['length'] / $stats['g']['cards'], 2);
-		$stats['g']['words-moy'] = round($stats['g']['length'] / $stats['g']['words'], 2);
 
-		$topCards = $db->prepare("SELECT * FROM {$config['database']['table']} ORDER BY CHAR_LENGTH(text) DESC LIMIT 10");
-		$topCards->execute();
-		while ($topCard = $topCards->fetch()) {
-			$topCard['stat-strlen'] = strlen($topCard['text']);
-			$topCard['stat-strlen_tot'] = round(( $topCard['stat-strlen'] / $stats['g']['length'] ) * 100, 1);
-$stats['c'] .= <<<TOPCARDSTAT
-<li>
-	<dl>
-		<dt><u>{$topCard['name']}</u></dt>
-		<dd>
-			<span style="color:#003399;">{$topCard['stat-strlen']}</span> caractères (<span style="color:#003399;">{$topCard['stat-strlen_tot']}</span>% total)
-		</dd>
-	</dl>
-</li>
-TOPCARDSTAT;
-		}
-
-		foreach ($stats['g']['t'] as $key => $value) {
-			$moy = round(( $value / $stats['g']['length'] ) * 100, 1);
-			if (isset($config['types'][$key])) $name = ucfirst($config['types'][$key]);
-			else $name = $key;
-$stats['t'] .= <<<TOPCARDSTAT
-<li>
-	<dl>
-		<dt><u>$name</u></dt>
-		<dd>
-			<span style="color:#003399;">{$value}</span> caractères (<span style="color:#003399;">{$moy}</span>% total)
-		</dd>
-	</dl>
-</li>
-TOPCARDSTAT;
-		}
 		$content['card'] = <<<HOMEPAGEEDITMAIN
 <h1>{$lang['admin-title']}</h1><br>
 <input id="pass" value="{$_POST['pass']}" type="hidden">
@@ -101,37 +52,6 @@ TOPCARDSTAT;
 		<button class="submit" onclick="changeMainParam()">{$lang['send']}</button>
 	</div>
 	<div>
-	</div>
-	<div>
-		<h2>Statistiques</h2>
-		<div class="flexboxData">
-			<div>
-				<h2>Statistiques généraux</h2>
-				Total de fiches : {$stats['g']['cards']}<br>
-				Total de caractères: {$stats['g']['length']}<br>
-				Nombre total de mots : {$stats['g']['words']}<br>
-				Total d'images : {$stats['g']['img']}<br>
-				Total de vidéos YouTube : {$stats['g']['vid']}<br>
-				<br>
-				Moyenne de lettre par mots : {$stats['g']['words-moy']}<br>
-				Moyenne de caractères par fiche : {$stats['g']['length-moy']}<br>
-			</div>
-			<div>
-				<h2>Fiches les plus grandes</h2>
-				<ol type="1">
-					{$stats['c']}
-				</ol>
-			</div>
-			<div>
-				<h2>Classement des types</h2>
-				<ul>
-					{$stats['t']}
-				</ul>
-			</div>
-			<div>
-				<h2>???</h2>
-			</div>
-		</div>
 	</div>
 </div>
 HOMEPAGEEDITMAIN;
