7e7040e919149b1c650920b3f1b12a80b22017df
Olpouin <olpouin@live.fr>
2019-04-17 16:15:05 +0200

    🆕 Auto-scrolls sidenav

--- php/sidenav.php
+++ php/sidenav.php
@@ -3,27 +3,27 @@ if (isset($_GET['edit'])) {$editURL = "&edit";}
 else {$editURL = "";}
 $content['sidenav'] = '<span class="title"><a href="'.PATH.'/">'.$config['general']['site_name'].'</a> <button class="sidenav-button" onclick="closeNav();">&times; Fermer le menu</button></span>';
 foreach ($config['cardsList'] as $type => $groups) {
-	$selectedClass = "";
-	if (isset($_GET['type']) && isset($_GET['name'])) {
-		if (empty($_GET['name'])) {
-			if (urldecode($_GET['type']) == $type) $selectedClass = "selectedPage";
-		}
-	}
-	$content['sidenav'] .= '<ul class="type"><span><a class="'.$selectedClass.'" href="'.hrefGen($type).'">'.ucfirst($config['types'][$type]).'</a></span>';
+	if (isset($_GET['type'])) {
+		if ($type == $_GET['type']) $opened = 'open=""';
+		else $opened = 'open=""';
+	} //yeah, not really useful yet...
+	else $opened = 'open=""';
+	$content['sidenav'] .= '<details '.$opened.' class="type"><summary>'.ucfirst($config['types'][$type]).'</summary>';
 	foreach ($groups as $group => $cards) {
 		$content['sidenav'] .= "<ul><span>".$group."</span>";
 		foreach ($cards as $card => $data) {
 			if (($data['hidden'] == 0) OR (isset($_POST['pass']) && $checkPassword($_POST['pass']))) {
-				$selectedClass = "";
+				$selectedClass = $scrollTo = "";
 				if (isset($_GET['name'])) {
 					if (urldecode($_GET['name']) == $card) $selectedClass .= "selectedPage";
+					if ($card == $_GET['name']) $scrollTo = "<script>window.onload = function(){document.getElementById('sidenav-".$card."').scrollIntoView()}</script>";
 				}
 				if ($data['hidden'] == 1) $selectedClass .= " hiddenPage";
-				$content['sidenav'] .= '<li><a class="'.$selectedClass.'" href="'.hrefGen($type,$card).$editURL.'">'.htmlentities($card).'</a></li>';
+				$content['sidenav'] .= '<li><a id="sidenav-'.$card.'" class="'.$selectedClass.'" href="'.hrefGen($type,$card).$editURL.'">'.htmlentities($card).'</a></li>'.$scrollTo;
 			}
 		}
 		$content['sidenav'] .= '</ul>';
 	}
-	$content['sidenav'] .= '</ul>';
+	$content['sidenav'] .= '</details>';
 }
 ?>
